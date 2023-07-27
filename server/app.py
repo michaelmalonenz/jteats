3f3a0639d60ed9ff685ee7f8b6c6e48c29d9ffcd
Michael Malone <michael.j.b.malone@gmail.com>
2023-07-27 21:23:07 +1200

    Can log in with Auth0

--- server/app.py
+++ server/app.py
@@ -1,12 +1,13 @@
 from functools import wraps
 import json
 import logging
-from flask import Flask, render_template, g, session, redirect, jsonify, request
+from flask import Flask, render_template, g, session, redirect, request
+from flask_session import Session
 from flask_dotenv import DotEnv
 from sqlalchemy import create_engine
 from dbconfig import get_db_url
 
-#from authlib.integrations.flask_client import OAuth
+from authlib.integrations.flask_client import OAuth
 # from api import API_APP
 # from repository import DatabaseConnector, SettingsRepository
 # from repository.local import UserRepository
@@ -18,38 +19,44 @@ app = Flask(__name__)
 env = DotEnv()
 env.init_app(app)
 # app.register_blueprint(API_APP, url_prefix='/api')
-# oauth = OAuth(app)
+oauth = OAuth(app)
 
-# with open('./oauth-config.json') as inf:
-#     oauth_conf = json.load(inf)
+app.config['SESSION_TYPE'] = 'filesystem'
+server_session = Session(app)
 
-# auth0 = oauth.register(
-#     'auth0',
-#     client_id=oauth_conf['clientID'],
-#     client_secret=oauth_conf['clientSecret'],
-#     api_base_url='https://{}'.format(oauth_conf['domain']),
-#     access_token_url='https://{}/oauth/token'.format(oauth_conf['domain']),
-#     authorize_url='https://{}/authorize'.format(oauth_conf['domain']),
-#     client_kwargs={
-#         'scope': 'openid profile email',
-#     }
-# )
+with open('./oauth-config.json') as inf:
+    oauth_conf = json.load(inf)
+
+AUTH0_DOMAIN = oauth_conf['domain']
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=oauth_conf['clientID'],
+    client_secret=oauth_conf['clientSecret'],
+    api_base_url=f'https://{AUTH0_DOMAIN}',
+    access_token_url=f'https://{AUTH0_DOMAIN}/oauth/token',
+    authorize_url=f'https://{AUTH0_DOMAIN}/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+    server_metadata_url=f'https://{AUTH0_DOMAIN}/.well-known/openid-configuration'
+)
 
 
-# @app.route('/auth/callback')
-# def callback_handling():
-#     # Handles response from token endpoint
-#     auth0.authorize_access_token()
-#     resp = auth0.get('userinfo')
-#     userinfo = resp.json()
+@app.route('/auth/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
 
-#     # Store the user information in flask session.
-#     session['jwt_payload'] = userinfo
-#     session['profile'] = {
-#         'user_id': userinfo['sub'],
-#         'name': userinfo['name'],
-#         'picture': userinfo['picture']
-#     }
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
 
 #     UserRepository(g.db).upsert_external_user(
 #         userinfo['sub'],
@@ -57,12 +64,12 @@ env.init_app(app)
 #         userinfo['nickname'],
 #         userinfo['picture'])
 
-#     return redirect('/')
+    return redirect('/')
 
 
-# @app.route('/login')
-# def login():
-#     return auth0.authorize_redirect(redirect_uri=oauth_conf['callbackURL'])
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri=oauth_conf['callbackURL'])
 
 @app.before_request
 def connect_to_db():
@@ -74,23 +81,18 @@ def connect_to_db():
 #         g.user_settings = SettingsRepository(g.db).read_for_user(g.current_user.id)
 
 
-# def requires_auth(f):
-#     @wraps(f)
-#     def decorated(*args, **kwargs):
-#         if 'profile' not in session:
-#             return redirect('/login')
-#         return f(*args, **kwargs)
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'profile' not in session:
+            return redirect('/login')
+        return f(*args, **kwargs)
 
-#     return decorated
+    return decorated
 
-
-@app.errorhandler(404)
-def not_found_handler(e):
-    print(request.access_route)
-    return jsonify(error=str(e)), 404
 
 @app.route('/', methods=['GET'])
-# @requires_auth
+@requires_auth
 def index():
     return render_template('index.html')
 
