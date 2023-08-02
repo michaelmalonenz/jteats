06bda6e747bc6b659bcb258dcc2d543c349d2ba4
Michael Malone <michael.j.b.malone@gmail.com>
2023-08-02 15:52:23 +1200

    add name to menu item

--- server/models/menu_item.py
+++ server/models/menu_item.py
@@ -8,12 +8,20 @@ class MenuItem(Base):
     __tablename__ = "menu_items"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
     description: Mapped[Optional[str]] = mapped_column(String(500))
     menu_section_id: Mapped[int] = mapped_column(ForeignKey("menu_sections.id"))
     menu_section: Mapped["MenuSection"] = relationship(back_populates="menu_items")
 
     def to_viewmodel(self):
-        pass
+        return {
+            'id': self.id,
+            'description': self.description,
+            'name': self.name,
+            'menuSectionId': self.menu_section_id,
+            'menuId': self.menu_section.menu_id,
+        }
 
-    def from_viewmodel(self, **kwargs):
+    @staticmethod
+    def from_viewmodel(**kwargs):
         pass
